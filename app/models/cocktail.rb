dfca537bcf734b8d526b9cd1770b7f550bef91d0
Paul Kathmann <paul.kathmann.98@gmail.com>
2019-07-04 14:45:24 +0200

    fixed models

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,5 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
-  has_many :ingredients, through: :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
-  validates_uniqueness_of :user_id, scope: [:question_id]
 end
